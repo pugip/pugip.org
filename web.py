8d5c54d69b6749b1240751eb893c283ed75aa8d2
Brian K. Jones <bkjones@gmail.com>
2012-03-20 00:11:50 -0400

    Created a more useful landing page for the group, and a logo.

--- web.py
+++ web.py
@@ -4,13 +4,14 @@ website. It's a simple Flask 'hello world' app.
 
 """
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("home.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
